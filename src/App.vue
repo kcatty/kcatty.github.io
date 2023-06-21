daba6a98bc7eadf96e97e182ece7047e2aa7cf73
kcatty <kbielicka@outlook.com>
2023-06-21 21:39:39 +0200

    add background for first banner

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,6 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <h1 class="font-poppins mb-4 text-3xl font-bold text-blue-600 underline">
-    Tailwind CSS 3 with vue js 3
-  </h1>
   <button class="px-6 py-2 text-sm text-green-100 bg-indigo-600 rounded">
     {{ $t("welcome") }}
   </button>
   <router-view />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="flex min-h-screen items-center justify-center bg-silver px-16">
+    <div class="relative w-full max-w-lg">
+      <div
+        class="animate-blob absolute -left-4 top-0 rounded-full bg-blush opacity-80 mix-blend-multiply blur-xl filter h-16 w-16 md:h-32 md:w-32 lg:h-48 lg:w-48 xl:h-64 xl:w-64"
+      ></div>
+      <div
+        class="animate-blob animation-delay-2 absolute -right-4 top-0 rounded-full bg-secondary opacity-70 mix-blend-multiply blur-xl filter h-32 w-32 md:h-44 md:w-44 lg:h-52 lg:w-52 xl:h-72 xl:w-72"
+      ></div>
+      <div
+        class="animate-blob animation-delay-4 absolute -bottom-8 left-20 rounded-full bg-dark opacity-70 mix-blend-multiply blur-xl filter h-24 w-24 md:h-36 md:w-36 lg:h-48 lg:w-48 xl:h-60 xl:w-60"
+      ></div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
